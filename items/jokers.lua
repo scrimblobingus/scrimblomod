a3bd584cb5e4a221891ddc8d6b9a6c8bd7356dc0
scrimblo bingus <masimbadenola@gmail.com>
2025-05-30 09:52:12 -0700

    first oak test

--- items/jokers.lua
+++ items/jokers.lua
@@ -63,13 +63,13 @@ SMODS.Joker {
     name = "racejoker",
     atlas = "raceatlas",
     pos = {x = 0, y = 0},
-    config = {extra = {dollars = 1}},
+    config = {extra = {dollars = 5}},
     loc_txt = {
         name = "Robloxian",
         text = {
             "Gives {C:money}$#1#{}",
             "when {C:dark_edition}Foil{}",
-            "joker is triggered.",
+            "card orjoker is triggered.",
         }
     },
     rarity = 4,
@@ -87,6 +87,15 @@ SMODS.Joker {
                 message = "past of tooth!",
                 dollars = card.ability.extra.dollars
             }
+        end
+
+        if context.individual and context.cardarea == G.play then
+            if context.other_card.edition and context.other_card.edition.foil == true then
+                return {
+                    message = "past of tooth!",
+                    dollars = card.ability.extra.dollars
+                }
+            end
         end
     end
 }
@@ -109,7 +118,7 @@ SMODS.Joker {
         text = {
             "This joker gains {X:mult,C:white}X#1#{} Mult",
             "whenever a {C:dark_edition}Holographic{}",
-            "joker is triggered.",
+            "card joker is triggered.",
             "{C:inactive}(Currently {X:mult,C:white}X#2#{}{C:inactive} Mult){}",
         }
     },
@@ -130,11 +139,88 @@ SMODS.Joker {
             }
         end
 
+        if context.individual and context.cardarea == G.play then
+            if context.other_card.edition and context.other_card.edition.holo == true then
+                card.ability.extra.Xmult = card.ability.extra.Xmult + card.ability.extra.Xmultamt
+                return {
+                    message = "ENHANCE!"
+                }
+            end
+        end
+
         if context.joker_main and card.ability.extra.Xmult > 0 then
             return {
                 message = "NO SCORING MESSAGE!",
                 Xmult_mod = card.ability.extra.Xmult
             }
+        end
+
+
+    end
+}
+
+SMODS.Atlas {
+    key = "oakatlas",
+    path = "oakjoker.png",
+    px = 213,
+    py = 285
+}
+
+SMODS.Joker {
+    key = "oakjoker",
+    name = "oakjoker",
+    atlas = "oakatlas",
+    pos = {x = 0, y = 0},
+    config = {extra = {chips = 100, Xchips = 2, odds = 2, Xodds = 6}},
+    loc_txt = {
+        name = "zombieman6475",
+        text = {
+            "When a {C:dark_edition}Polychrome{}",
+            "card or joker triggers,",
+            "{C:green}#3# in #4#{} chance for {C:chips}+#1#{} Chips",
+            "{C:green}#3# in #5#{} chance for {X:chips,C:white}X#2#{} Chips"
+        }
+    },
+    rarity = 4,
+    cost = 15,
+    pools = {["gcmember"] = true},
+    unlocked = true,
+    discovered = true,
+    loc_vars = function(self, info_queue, center)
+        return {vars = {center.ability.extra.chips, center.ability.extra.Xchips, G.GAME.probabilties.normal or 1, center.ability.extra.odds, center.ability.extra.Xodds}}
+    end,
+
+    calculate = function(self, card, context)
+        if context.other_joker and context.other_joker.edition and context.other_joker.edition.polychrome == true then
+            if pseudorandom("oakchips") < G.GAME.probabilties.normal / card.ability.extra.odds then
+                return {
+                    message = "jolly!",
+                    chips = card.ability.extra.chips
+                }
+            end
+            if pseudorandom("oakxchips") < G.GAME.probabilties.normal / card.ability.extra.Xodds then
+                return {
+                    message = "jollier!",
+                    Xchip_mod = card.ability.extra.Xchips
+                }
+            end
+        end
+
+        if context.individual and context.cardarea == G.play then
+            if context.other_card.edition and context.other_card.edition.polychrome == true then
+                if pseudorandom("oakchips") < G.GAME.probabilties.normal / card.ability.extra.odds then
+                    return {
+                        message = "jolly!",
+                        chips = card.ability.extra.chips
+                    }
+                end
+                if pseudorandom("oakxchips") < G.GAME.probabilties.normal / card.ability.extra.Xodds then
+                    return {
+                        message = "jollier!",
+                        Xchip_mod = card.ability.extra.Xchips
+                    }
+                end
+            end
         end
     end
 }
